be89c1b9d79b40f4d5b6bcea1a42ec4a8eb145c4
Kamil Sawicki <sawicki_kamil@onet.pl>
2020-01-14 22:50:51 +0100

    adding Repository

---
+++ src/main/java/com/Spring/springApp/repositories/BookRepository.java
@@ -0,0 +1,8 @@
+package com.Spring.springApp.repositories;
+
+import com.Spring.springApp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+    
+}
